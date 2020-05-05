8d1e5b386afd57de5150e27a478be0d1b5a7967d
Rami Shahatit <rshahatit@berkeley.edu>
2020-05-04 22:57:55 -0700

    Updates
    Change-Id: If7bab177d3e322dc42fa6ad84bfe25354fa506f4

---
+++ component---src-pages-404-js-e5926e9c00e41d02113a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{nil9:function(t,e,n){t.exports=n.p+"static/rashmi-790713cc35160b661518faf68d0d475c.svg"},w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),s=n.n(a),i=n("nil9"),c=n.n(i);e.default=function(){return s.a.createElement("div",null,s.a.createElement("img",{src:c.a,alt:"rashmi ap"}),s.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e5926e9c00e41d02113a.js.map
--- src/data/Cert.js
+++
@@ -1,7 +0,0 @@
-export default [
-    {
-        id: 1,
-        name: "GIAC Certifications",
-        certification: "GCIH Incident Handler"
-    },
-];
